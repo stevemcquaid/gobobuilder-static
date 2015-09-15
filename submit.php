eea970c68287faa6786983f17a2b84ade56afb12
Stephen McQuaid <smcquaid@godaddy.com>
2015-09-15 15:14:05 -0700

    Put secret keys in the secret.json file

--- submit.php
+++ submit.php
@@ -2,8 +2,7 @@
 
 require 'MCAPI.class.php';
 
-$config = json_decode(file_get_contents('config.json'), true);
-
+$config = array_merge_recursive(json_decode(file_get_contents('config.json'), true), json_decode(file_get_contents('secret.json'), true));
 $api = new MCAPI($config['mailchimp']['api_key']);
 
 $api->listSubscribe($config['mailchimp']['list_id'], $_POST['email'], array('FNAME' => $_POST['fname'], 'LNAME' => $_POST['lname']));
